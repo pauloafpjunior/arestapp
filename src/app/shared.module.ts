f7f662280bc1f4728d1926407ea07491eb863f91
Paulo Jr <pauloa.junior@ufla.br>
2021-10-13 16:46:20 -0300

    Criando componente de cabeçalho

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [CommonModule, FormsModule, IonicModule],
-  exports: [CommonModule, FormsModule, IonicModule],
+  exports: [CommonModule, FormsModule, IonicModule, HeaderComponent],
 })
 export class SharedModule {}
